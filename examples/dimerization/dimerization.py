433f1104da305a67cfe7ccb6f6e66183d4a1606a
Andreas Hellander <andreash@Andreas-Hellanders-MacBook-Pro.local>
2013-02-03 22:07:37 -0800

    progress on Cartesian 1D simple example

--- examples/dimerization/dimerization.py
+++ examples/dimerization/dimerization.py
@@ -3,8 +3,7 @@ import os
 from pyurdme.urdme import *
 
 def dimerization(model_name=""):
-    """ Dimerization. The reversible reaction A+B<->C on a
-        surface. """
+    """ Dimerization. The reversible reaction A+B<->C on a surface. """
     if model_name == "":
         model_name = "dimerization"
     model = URDMEModel(name=model_name);
@@ -27,6 +26,16 @@ def dimerization(model_name=""):
     R2 = Reaction(name="R2",reactants={C:1},products={A:1,B:1},massaction=True,rate=k2)
     model.addReaction([R1,R2])
 
+    # A square domain with Cartesian discretization
+    model.mesh  = CartesianMesh(geometry="line",side_length=1e-6,hmax=1e-7)
+    #model.mesh = importmesh('meshes/surface.msh')  
+    model.meshextend()
+    # self.D = assemble(model)
+
+    #vol,D = model.assemble()
+    #model.vol = vol
+    #model.D = D
+    
     # Import a Gmsh mesh and physical regions (subdomains)
     # mesh = dolfin.Mesh('meshes/surface.xml')
     # sd = dolfin.MeshFunction('meshes/surface_physical_region.xml')
@@ -34,14 +43,14 @@ def dimerization(model_name=""):
     # model.mesh = mesh
     # Create extended mesh (Will this be necessary?) for use with URDME
     # model.meshextend()
-    
     model.num_voxels  = 1000
     model.num_species = len(model.listOfSpecies)
     model.num_reactions = len(model.listOfReactions)
-    model.meshextend()
+    #model.meshextend()
 
     model.InitInitialValue()
     model.initializeSubdomainVector()
+    
     # Distribute the Species' initial values over the mesh
     model.scatter(A,subdomain=2)
     model.scatter(B,subdomain=2)
@@ -59,4 +68,4 @@ if __name__ == '__main__':
     model.serialize(filename='testdimerization.mat')
     
     # Run URDME
-    urdme(model)
+    #urdme(model)

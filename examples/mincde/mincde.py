b68c2859c77a19fc1f74fd55d9decd73f3d7d09d
Andreas Hellander <andreash@crocodile.(none)>
2013-02-05 16:48:41 -0800

    progress on a mincde.py model file

--- examples/mincde/mincde.py
+++ examples/mincde/mincde.py
@@ -4,19 +4,32 @@ import os
 from pyurdme.urdme import *
 
 def mincde(model_name=""):
-	
+
 	if model_name == "":
-        model_name = "dimerization"
-    model = URDMEModel(name=model_name);
+		model_name = "dimerization"
+	model = URDMEModel(name=model_name)
 
 	# Species
-    MinD_m = Species(name="MinD_m",initial_value=0,diffusion_constant=1e-14);
-    MinD_c_atp = Species(name="MinD_c_atp",initial_value=0,diffusion_constant=2.5e-12);
-    MinD_c_adp = Species(name="MinD_c_adp",initial_value=4500,diffusion_constant=2.5e-12);
-    MinD_e = Species(name="MinD_e",initial_value=1575,diffusion_constant=2.5e-12);
-    MinDE = Species(name="MinDE",initial_value=0,diffusion_constant=1e-14);
+	MinD_m = Species(name="MinD_m",initial_value=0,diffusion_constant=1e-14);
+	MinD_c_atp = Species(name="MinD_c_atp",initial_value=0,diffusion_constant=2.5e-12);
+	MinD_c_adp = Species(name="MinD_c_adp",initial_value=4500,diffusion_constant=2.5e-12);
+	MinD_e = Species(name="MinD_e",initial_value=1575,diffusion_constant=2.5e-12);
+	MinDE = Species(name="MinDE",initial_value=0,diffusion_constant=1e-14);
+	model.addSpecies([MinD_m,MinD_c_atp,MinD_c_adp,MinD_e,MinDE])
 	
 	# Parameters 
-	
+	sigma_d = Parameter(name="sigma_d",expression=2.5e-8)
+    sigma_dD = Parameter(name="sigma_dD",expression=0.0016e-18)
+    sigma_e = Parameter(name="sigma_e",expression=0.093e-18)
+    sigma_de = Paramter(name="sigma_de",expression=0.7)
+    sigma_dt = Parameter(name="sigma_dt",expression=1.0)
+    model.addParameter([sigma_d,sigma_dD,sigma_e,sigma_de,sigma_dt])
+
 	# Reactions
-	
+
+	return model
+
+if __name__=="__main__":
+	""" Dump model to a file. """
+	model = mincde(model_name="mincde")	
+	#model.serialize(filename="mincdeinput.mat")
--- pyurdme/gmsh.py
+++ pyurdme/gmsh.py
@@ -1,4 +1,31 @@
 """ Wrappers for selected Gmsh functionality. """
+import subprocess
+
+class GmshGeo():
+	""" Represent a Gmsh geometry. Wrapper arounf a
+	    Gmesh .geo script file. """			
+	def __init__(self,geofile=None):
+		self.geofile = geofile
+		
+				
+class GmshMesh():
+
+	def __init__(self,meshfile=None):
+		importmesh(meshfile)	
+		
+	def refine(self):
+		""" Refine the mesh. """	
+	
+def imoportmesh(meshfile):
+	""" Import a Gmsh .msh file. """	
+	# TBD. 
 
 
+def meshinit(geom):
+	""" Call Gmsh and initialize a mesh from the .geo file. """
+	# TBD	
+	subprocess.call(['gmsh -m '])
 
+def toDolfin(mesh):
+	""" Serialize a Gmsh mesh to Dolfin format"""
+	# TBD.  	

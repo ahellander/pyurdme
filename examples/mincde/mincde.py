5a8c4692fe2efdead11de49ec6bcafa23c197d06
ahellander <andreash@cs.ucsb.edu>
2013-04-11 15:39:49 -0700

    Added initial mincde model

--- examples/mincde/mincde.py
+++ examples/mincde/mincde.py
@@ -29,7 +29,8 @@ def mincde(model_name=""):
 	model.addParameter([sigma_d,sigma_dD,sigma_e,sigma_de,sigma_dt])
 
 	# Reactions
-	# TODO: These reactions will serialize without volume dependency. This needs to be adressed in the model.py module
+	# TODO: These reactions will serialize without volume dependency. This needs to be adressed in the model.py module.
+    # How do we get the voxel volumes into the propensity functions in the .c model file ?
 	R1 = Reaction(name="R1",reactants={MinD_c_atp:1},products={MinD_m:1},massaction=True,rate=sigma_d)
 	R2 = Reaction(name="R2",reactants={MinD_c_atp:1,MinD_m:1},products={MinD_m:2},massaction=True,rate=sigma_dD)
 	R3 = Reaction(name="R3",reactants={MinD_m:1,MinD_e:1},products={MinDE:1},massaction=True,rate=sigma_e)
@@ -46,5 +47,6 @@ def mincde(model_name=""):
 if __name__=="__main__":
 	""" Dump model to a file. """
 	model = mincde(model_name="mincde")	
-	urdme(model)
+	# TODO: This does not work yet. 
+	#urdme(model)
 	

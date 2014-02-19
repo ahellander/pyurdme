92dd32acc2891d871f9ad241975df571c50cbd5b
Andreas Hellander <andreash@Andreass-MacBook-Pro-2.local>
2014-02-19 22:54:16 +0100

    removed examples fron install

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name="pyurdme",
       version="1.0.0",
       author="Andreas Hellander, Brian Drawert",
       author_email="andreas.hellander@gmail.com",
-      packages=['pyurdme','examples','examples/mincde','examples/cylinder_demo'],
-      package_data={'pyurdme':['data/*','urdme/*','urdme/bin/*','urdme/build/*','urdme/include/*','urdme/src/*','urdme/src/nsm/*','urdme/src/dfsp/*'],'examples/mincde':['mesh/*']})
+      packages=['pyurdme'],
+      package_data={'pyurdme':['data/*','urdme/*','urdme/bin/*','urdme/build/*','urdme/include/*','urdme/src/*','urdme/src/nsm/*','urdme/src/dfsp/*']})

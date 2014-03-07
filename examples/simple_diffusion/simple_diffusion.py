10489c19432057247cf892f6fefba0e35deaa030
Andreas Hellander <andreash@Andreass-MacBook-Pro-2.local>
2014-03-07 10:17:09 +0100

    Patched up toVTK, fixed bug that prevented models with only one species from working properly.

--- examples/simple_diffusion/simple_diffusion.py
+++ examples/simple_diffusion/simple_diffusion.py
@@ -1,24 +1,25 @@
 """ pyurdme model for a single species diffusing on the unit square. """
 
 import dolfin
-from pyurdme.pyurdme import *
+import pyurdme
+import numpy
 
-class simple_diffusion(URDMEModel):
+class simple_diffusion(pyurdme.URDMEModel):
     """ Initial condition is a delta function at the center voxel. 
         The solution should be a Gaussian, up to the point where
         the BC becomes important. """
 
     def __init__(self):
     
-        URDMEModel.__init__(self,name="simple_diffusion")
+        pyurdme.URDMEModel.__init__(self,name="simple_diffusion")
 
         D = 0.01
-        A = Species(name="A",diffusion_constant=D,dimension=2)
+        A = pyurdme.Species(name="A",diffusion_constant=D,dimension=2)
 
         self.addSpecies([A])
 
         # A unit square
-        self.mesh = unitSquareMesh(40,40)
+        self.mesh = pyurdme.Mesh.unitSquareMesh(40,40)
                 
         # Place the A molecules in the voxel nearest the center of the square
         self.placeNear({A:100000},point=[0.5,0.5])
@@ -29,15 +30,11 @@ class simple_diffusion(URDMEModel):
 if __name__ == '__main__':
 
     model = simple_diffusion()
-    
-    #solver_options = {'tspan':numpy.linspace(0,1,50),
-    #                   'seed':1432423}
-    
-    #solver_data = model.solverData()
-
-    result = urdme(model)
-
+    result = pyurdme.urdme(model)
+    data = model.solverData()
+    result.toTHREEJs("A", 20)
     # Dump a snapshot of the state in paraview format
-    file = dolfin.File("A.pvd")
-    file << model.sol['A']
+    result.toVTK("A", "output")
+    #file = dolfin.File("A.pvd")
+    #file << model.sol['A']
 

9fc4522e57578b00c779cc0b36937d125e2632c8
Brian Drawert <bdrawert@cs.ucsb.edu>
2014-03-09 23:13:45 -0700

    connectivity tests not working out

---
+++ examples/yeast_polarization/connectivity_test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+from dolfin import *
+import dolfin
+dolfin.parameters["linear_algebra_backend"] = "uBLAS"
+import matplotlib.pyplot as plt
+import scipy.sparse
+
+
+
+# Sub domain for Periodic boundary condition
+class PeriodicBoundary1D(SubDomain):
+
+    # Left boundary is "target domain" G
+    def inside(self, x, on_boundary):
+        # return True if on left or bottom boundary AND NOT on one of the two corners (0, 1) and (1, 0)
+        print "PeriodicBoundary1D.inside(x={0}) = {1}".format(x[0], not bool((near(x[0], 0)) and on_boundary))
+        return not bool((near(x[0], 0)) and on_boundary)
+
+    def map(self, x, y):
+        print "PeriodicBoundary1D.map(x={0}, y={0}) y={0}".format(x, y)
+        if near(x[0], 0):
+            y[0] = x[0] + 1.
+            print "\tnew y = {0}".format(y)
+        else:
+            print "\tnew y = {0}".format(y)
+
+
+
+if __name__ == "__main__":
+    mesh = UnitIntervalMesh(6)
+
+    fs = dolfin.FunctionSpace(mesh, "Lagrange", 1, constrained_domain=PeriodicBoundary1D())
+    #fs = dolfin.FunctionSpace(mesh, "Lagrange", 1)
+    trial_function = dolfin.TrialFunction(fs)
+    test_function = dolfin.TestFunction(fs)
+    a_K = -1*dolfin.inner(dolfin.nabla_grad(trial_function), dolfin.nabla_grad(test_function)) * dolfin.dx
+    C = dolfin.assemble(a_K)
+    rows, cols, vals = C.data()
+    C = scipy.sparse.csr_matrix((vals, cols, rows))
+    C = C.tocsc()
+
+    print C
+    print mesh.coordinates()
+    plt.spy(C)
+    plt.title("1D connectivity, with periodic BCs")
+    plt.show()
+

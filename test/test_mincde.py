97ad4f928e108c12f11c8b73c43d1ca2cecf78f5
Brian Drawert <bdrawert@cs.ucsb.edu>
2014-04-29 04:03:45 -0700

    fixed mincde unit test

--- test/test_mincde.py
+++ test/test_mincde.py
@@ -17,11 +17,13 @@ class TestMinCDE(unittest.TestCase):
         model = mincde()
         result = model.run()
         mindm = result.get_species("MinD_m")
+        y_vals = model.mesh.coordinates()[:, 1]
+        idx = (y_vals < 1e-6)
         mindmsum = numpy.sum(mindm[:,idx],axis=1)
-        mindfft = scipy.fftpack.fft(mindmsum[200:]-mean(mindmsum[200:]))
+        mindfft = scipy.fftpack.fft(mindmsum[200:]-numpy.mean(mindmsum[200:]))
         N = len(mindfft)
         T = model.tspan[-1] - model.tspan[200]
-        mindpsd = numpy.abs(mindfft[:floor((N-1)/2)]) 
+        mindpsd = numpy.abs(mindfft[:numpy.floor((N-1)/2)]) 
         mindfreq = numpy.arange(len(mindpsd), dtype=float)/T
         mind_max_period = 1/mindfreq[1+numpy.argmax(mindpsd[1:])]
         self.assertTrue(mind_max_period > 60)

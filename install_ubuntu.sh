2ec67f08e442d0bcf25f9274ee426ffaf55b9935
Andreas Hellander <andreash@dhcp-17-72.it.uu.se>
2014-04-30 14:42:52 +0200

    Fixed mistype

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -2,7 +2,7 @@
 apt-get -y update
 apt-get -y install git
 apt-get -y install build-essential python-dev
-pt-get -y install python-setuptools
+apt-get -y install python-setuptools
 apt-get -y install python-matplotlib python-numpy python-scipy
 apt-get -y install make
 apt-get -y install python-software-properties

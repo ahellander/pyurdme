810441d3086d728d29bbad8d5d33509132a1b838
Andreas Hellander <andreash@squirrel.(none)>
2014-04-30 09:25:43 +0200

    Added setup dep

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ setup(name="pyurdme",
       package_data={'pyurdme':['data/*.c','data/three.js_templates/js/*','data/three.js_templates/*.html','urdme/AUTHORS','urdme/LICENCE','urdme/bin/*','urdme/build/*','urdme/include/*','urdme/src/*.c','urdme/src/nsm/*']},
       
       install_requires = ["matplotlib","numpy","scipy","h5py","Jinja2"],
+      setup_requires = ["distribute >=0.6.28"]
       
       author="Andreas Hellander, Brian Drawert",
       author_email="andreas.hellander@gmail.com",
-      licence = "GPL",
+      license = "GPL",
       keywords = "pyurdme, urdme, spatial stochastic simulation, rdme",
       url = ""
       

7325fe10f742058413e20c4517da38bbfce3d653
shellander <st.hellander@gmail.com>
2016-08-23 20:08:34 -0700

    installscript

--- pyurdme/mmms/installscript.sh
+++ pyurdme/mmms/installscript.sh
@@ -22,7 +22,13 @@ jupyter notebook --generate-config
 
 sed -i "1s/^/c = get_config()\nc.IPKernelApp.pylab = 'inline'\nc.NotebookApp.ip = '*'\nc.NotebookApp.open_browser = False\nc.NotebookApp.port = 8889\n/" /root/.jupyter/jupyter_notebook_config.py
 
-cd /home/pyurdme/pyurdme/mmms/hdf5-1.8.16/
+
+apt-get install wget
+wget http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.17.tar
+tar xvf hdf5-1.8.17.tar
+rm hdf5-1.8.17.tar
+cd /home/pyurdme/pyurdme/mmms/hdf5-1.8.17/
+./configure --enable-cxx
 make
 make install
 
@@ -33,4 +39,4 @@ make
 source /home/pyurdme/pyurdme_init
 
 cd /home/pyurdme/examples/micro1
-jupyter notebook
+#jupyter notebook
